25ea01c832f0e74562a0572016f06611b1679fc0
Darren Liew <ldarren@gmail.com>
2023-07-21 02:18:06 +0800

    working pg

--- api/mod/pg_pool.js
+++ api/mod/pg_pool.js
@@ -4,15 +4,21 @@ let pool
 
 module.exports = {
 	setup(cfg){
+		const config = Object.assign({}, cfg)
+		config.port = parseInt(cfg.port)
 		pool = new Pool(cfg)
 	},
 	user: {
 		// return error if ghuser already exist
 		async validate(ghuser){
+			const res = await pool.query('SELECT * FROM accounts WHERE id = $1 and type = $2 and s = $3;', [ghuser.login, 'github', 1])
+			const user = res.rows[0]
+			if (user) return this.next({status: 400})
 			return this.next()
 		},
 		// create new user
 		async save(ghuser, cred, output){
+			await pool.query('INSERT INTO accounts (id, type, user_id, credentials) VALUES ($1, $2, $3, $4);', [ghuser.login, 'github', 0, JSON.stringify(cred)])
 			return this.next()
 		}
 	}
